5a9d3bc8481ca1f1bd236e3fa72df00ced452b5c
Amaan-1 <66007400+Amaan-1@users.noreply.github.com>
2020-11-16 17:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function para_show() {
     for (var i = 1; i <= 6; i++) {
         input.push(document.getElementById("thing-" + i).value);
     }
-    document.getElementById("para-show-1").innerHTML = input.join(". ");
+    var stop = input.join(". ");
+    console.log(stop);
+    document.getElementById("para-show-1").innerHTML = stop;
 }
 
 function para_show_2() {
@@ -11,6 +13,7 @@ function para_show_2() {
     for (var j = 1; j <= 6; j++) {
         input_2.push(document.getElementById("stuff-" + j).value);
     }
-    var stop = input_2.join(". ");
-    document.getElementById("para-show-2").innerHTML = stop;
+    var stop_2 = input_2.join(". ");
+    console.log(stop_2);
+    document.getElementById("para-show-2").innerHTML = stop_2;
 }
